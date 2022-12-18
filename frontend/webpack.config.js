e2143e41cd7bbdeaa68918f531b212ad6de99f24
YoungHo Choi <0505zxc@gmail.com>
2022-12-18 21:14:50 +0900

    🚀 1단계 - 화면 응답 개선하기 (#16)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,15 @@
 const commonConfig = require('./webpack.common.js')
 const webpackMerge = require('webpack-merge')
 const { argv } = require('yargs')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = () => {
   const envConfig = require(`./webpack.${argv.env}.js`)
   return webpackMerge(commonConfig, envConfig)
 }
+
+module.exports = {
+    configureWebpack: {
+        plugins: [new BundleAnalyzerPlugin()]
+    }
+};
