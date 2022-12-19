6feea8fc57154ea9ecba664076fc2d3db2e1dcf3
OhSeongCheol <dhtmdcjf7@gmail.com>
2022-12-19 10:53:34 +0900

    [M4] feat: 웹 캐시 추가

---
+++ src/main/java/nextstep/subway/WebMvcConfig.java
@@ -0,0 +1,46 @@
+package nextstep.subway;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl jsCacheControl = CacheControl
+                .noCache()
+                .cachePrivate();
+
+        registry
+                .addResourceHandler("/**/*.js")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(jsCacheControl);
+
+        CacheControl cssCacheControl = CacheControl
+                .maxAge(365, TimeUnit.DAYS);
+
+        registry
+                .addResourceHandler("/**/*.css")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(cssCacheControl);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter etagHeaderFilter = new ShallowEtagHeaderFilter();
+        registration.setFilter(etagHeaderFilter);
+        registration.addUrlPatterns("/**");
+        return registration;
+    }
+}
