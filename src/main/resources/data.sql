cc29f00bd01b46ada0ef73e2649d525b764bee92
OhSeongCheol <dhtmdcjf7@gmail.com>
2022-12-19 09:11:30 +0900

    [M4] config: DB 초기 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into member (id, created_date, modified_date, age, email, password) values (1, NOW(), NOW(), 29, 'mail@mail', 29)
+
+insert into station (id, created_date, modified_date, name) values (1, NOW(), NOW(), 'v1')
+insert into station (id, created_date, modified_date, name) values (2, NOW(), NOW(), 'v2')
+insert into station (id, created_date, modified_date, name) values (3, NOW(), NOW(), 'v3')
+insert into station (id, created_date, modified_date, name) values (4, NOW(), NOW(), 'v4')
+insert into station (id, created_date, modified_date, name) values (5, NOW(), NOW(), 'v5')
+insert into station (id, created_date, modified_date, name) values (6, NOW(), NOW(), 'v6')
+insert into station (id, created_date, modified_date, name) values (7, NOW(), NOW(), 'v7')
+insert into station (id, created_date, modified_date, name) values (8, NOW(), NOW(), 'v8')
+insert into station (id, created_date, modified_date, name) values (9, NOW(), NOW(), 'v9')
+insert into station (id, created_date, modified_date, name) values (10, NOW(), NOW(), 'v10')
+
