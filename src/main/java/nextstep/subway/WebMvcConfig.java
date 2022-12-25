d390a1c4e8634b4bec0a028a5d1814b720e24e5e
wu2ee <gulb1602@gmail.com>
2022-12-25 16:20:50 +0900

    feat : http cache 적용
    - css 파일 없어서 테스트를 위한 main.css 추가

---
+++ src/main/java/nextstep/subway/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package nextstep.subway;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
+
+        registry.addResourceHandler("/**/*.css")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter etagHeaderFilter = new ShallowEtagHeaderFilter();
+        registration.setFilter(etagHeaderFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+}
